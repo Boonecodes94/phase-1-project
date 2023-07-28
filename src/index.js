4a04113f50b295b3112fe8746f1245672e3d2b06
sandra ogbudinkpa <sandra.os325@gmail.com>
2023-07-28 07:04:55 -0400

    makes changes to js, to fetch options from json when personal or recreational button is pressed. renders data to page

--- src/index.js
+++ src/index.js
@@ -4,14 +4,42 @@ const baseUrl = 'http://localhost:3000';
 const optionsUrl = baseUrl + '/options'
 const favoriteUrl = baseUrl + '/favorites'
 
+
+const categoryButtons = document.getElementById('category').children;
+const list1 = document.getElementById('list-1');
+const list2 = document.getElementById('list-2');
+const recommendationText = document.getElementById('recommendation-text');
+const eventOptionImage = document.getElementById('event-option-image');
+
 // Fetch options 
 //when Personal or Recreational button is clicked
-document.getElementById('personal-button').addEventListener('click', () => fetchOptions('personal'));
-document.getElementById('recreational-button').addEventListener('click', () => fetchOptions('recreational'));
+function fetchOptions(category) {
+    fetch(optionsUrl + '?category=' + category)
+        .then(response => response.json())
+        .then(data => renderOptions(data));
+}
 
+//  Render data to the page
+function renderOptions(options) {
+    // Clear previous options
+    while(list1.firstChild) list1.firstChild.remove();
+    while(list2.firstChild) list2.firstChild.remove();
 
+    for(let i = 0; i < options.length; i++) {
+        const optionButton = document.createElement('button');
+        optionButton.textContent = options[i].name;
+        optionButton.addEventListener('click', () => makeRecommendation(options[i]));
+        if(i < 2) {
+            list1.appendChild(optionButton);
+        } else {
+            list2.appendChild(optionButton);
+        }
+    }
+}
 
-
+for(let i = 0; i < categoryButtons.length; i++) {
+    categoryButtons[i].addEventListener('click', () => fetchOptions(categoryButtons[i].textContent.toLowerCase()));
+}
 // event listeners, should make changes to the page as required
 function makeRecommendation(option) {
     recommendationText.textContent = `Based on your choices, you should do ${option.name}`;
