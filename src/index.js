2d8673b0efe5cdfd751fce4b305840b2c9d1b88d
sandra ogbudinkpa <sandra.os325@gmail.com>
2023-07-27 22:47:35 -0400

    adds event listeners

--- src/index.js
+++ src/index.js
@@ -9,3 +9,22 @@ const favoriteUrl = baseUrl + '/favorites'
 document.getElementById('personal-button').addEventListener('click', () => fetchOptions('personal'));
 document.getElementById('recreational-button').addEventListener('click', () => fetchOptions('recreational'));
 
+
+
+
+// event listeners, should make changes to the page as required
+function makeRecommendation(option) {
+    recommendationText.textContent = `Based on your choices, you should do ${option.name}`;
+    eventOptionImage.src = option.image;
+    saveFavorite(option);
+}
+
+function saveFavorite(option) {
+    fetch(favoriteUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(option),
+    });
+}
