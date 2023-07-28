0f1dc35595eaf5793a452aade587ee4145088b60
sandra ogbudinkpa <sandra.os325@gmail.com>
2023-07-27 22:00:32 -0400

    starts js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+// The base URL to the backend server 
+// The endpoints to get options and save favorites
 const baseUrl = 'http://localhost:3000';
+const optionsUrl = baseUrl + '/options'
+const favoriteUrl = baseUrl + '/favorites'
+
+// Fetch options 
+//when Personal or Recreational button is clicked
+document.getElementById('personal-button').addEventListener('click', () => fetchOptions('personal'));
+document.getElementById('recreational-button').addEventListener('click', () => fetchOptions('recreational'));
